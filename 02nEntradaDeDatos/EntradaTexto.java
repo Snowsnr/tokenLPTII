3c25c7fe444b192ee5640062f7d0cdf9a84c5bcc
Snowsnr <dreyess2104@alumno.ipn>
2022-09-07 17:50:32 -0500

    actualizacion de dato entero

--- 02nEntradaDeDatos/EntradaTexto.java
+++ 02nEntradaDeDatos/EntradaTexto.java
@@ -1,27 +1,35 @@
 //esto es un comentario
 
 /**
- * Todo programa en java debe tener el nombre de la clase tal cual como el archivo
+ * Todo programa en java debe tener el nombre de la clase tal cual como el
+ * archivo
  */
 
- class EntradaTexto{
-    //los limites de la clase
+class EntradaTexto {
+   // los limites de la clase
 
-    //toda la clase debe tener un metodo principal
-    //**Vamos a realizar un programa mediante el cual podemos introducir texto a nuestra conveniencia */
-    public static void main(String[] args) {
-    //tipo de dato
-    String nombre;
-    //el ; es el fin de la linea
-    //Instancia
+   // toda la clase debe tener un metodo principal
+   // **Vamos a realizar un programa mediante el cual podemos introducir texto a
+   // nuestra conveniencia */
+   public static void main(String[] args) {
+      // tipo de dato
+      String nombre;
+      // el ; es el fin de la linea
+      int edad;
+      // Instancia
 
-    System.out.println("Por favor introducte tu nombre asi bien kawaii");
+      System.out.println("Por favor introducte tu nombre asi bien kawaii");
 
-    //vamos a obtener el nombre del usuario
+      // vamos a obtener el nombre del usuario
 
-    nombre = System.console().readLine();
+      nombre = System.console().readLine();
+      System.out.println("Hola " + nombre + " bienvenido a tu programita");
 
-    System.out.print("Hola "+ nombre + " bienvenido a tu programita");
-    }
+      System.out.println("Por favor introduce tu edad");
 
- }
+      edad = Integer.parseInt(System.console().readLine());
+
+      System.out.println("Hola " + nombre + " bienvenido a tu programita tienes " + edad);
+   }
+
+}
